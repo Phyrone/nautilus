6473d38d64f36997930c2116367250672ab9ab6c
Phyrone <phyrone@phyrone.de>
2024-10-05 22:57:18 +0200

    add grpc builder for rust

---
+++ lib/grpc/build.rs
@@ -0,0 +1,27 @@
+fn main() {
+    println!("cargo:rerun-if-changed=../../proto");
+
+    let builder = tonic_build::configure();
+    let builder = builder.compile_well_known_types(true)
+        .use_arc_self(true)
+        .build_client(true)
+        .build_server(true)
+        .build_transport(true)
+        .emit_rerun_if_changed(false)
+        .include_file("_all.rs")
+        .out_dir("src/main/rust/");
+
+    //get proto files from ../../proto
+    let files = walkdir::WalkDir::new("../../proto")
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.path().extension().map_or(false, |e| e == "proto"))
+        .map(|e| e.path().to_path_buf())
+        .collect::<Vec<_>>();
+
+
+    builder.compile_protos(
+        &files,
+        &["../../proto"],
+    ).expect("Failed to compile protos");
+}
