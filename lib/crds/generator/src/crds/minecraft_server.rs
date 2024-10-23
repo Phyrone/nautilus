0d24315f5a64fa0982fa5d67e6afb78271f7847b
Phyrone <phyrone@phyrone.de>
2024-10-24 01:06:18 +0200

    ✨Add install features + Add memory limit options

--- lib/crds/generator/src/crds/minecraft_server.rs
+++ lib/crds/generator/src/crds/minecraft_server.rs
@@ -1,4 +1,6 @@
+use k8s_openapi::api::apps::v1::DeploymentStrategy;
 use k8s_openapi::api::core::v1::ObjectReference;
+use k8s_openapi::apimachinery::pkg::util::intstr::IntOrString;
 use kube::CustomResource;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -16,10 +18,17 @@ use serde::{Deserialize, Serialize};
 pub struct MinecraftServerV1Alpha1Spec {
     #[schemars(range(min = 0))]
     replicas: Option<i32>,
+    deployment_strategy: Option<DeploymentStrategy>,
     /// The base image to use for the server
     /// By default, itzg/minecraft-server is used.
     /// This does not affect the provisioner service.
     image: Option<String>,
+    //TODO settings (properties etc.)
+    //TODO env (additional environment variables)
+    //TODO additional volumes, mounts, ports, etc.
+    //TODO jvm settings (memory, aikar flags, etc.)
+    resources: Option<MinecraftServerResourcesSpec>,
+
     template: Option<MinecraftTemplateSpec>,
     install: Option<MinecraftServerInstallsSpec>,
     persistence: Option<MinecraftServerPersistence>,
@@ -54,9 +63,77 @@ pub struct GitTemplate {
 
 #[derive(Debug, Clone, Deserialize, Serialize, JsonSchema)]
 #[serde(rename_all = "camelCase")]
+pub struct MinecraftServerResourcesSpec {
+    pub memory: Option<MineecraftServerResourcesMemorySpec>,
+
+    //TODO cpu settings
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct MineecraftServerResourcesMemorySpec {
+    /// The max heap size for the JVM.
+    /// This is the amount of memory the JVM can use for the heap.
+    /// The JVM will use more memory than this value. The exact amount depends on the JVM implementation.
+    ///
+    /// The value can be a number in bytes or a string with a unit.
+    /// An empty string means no limit.
+    ///
+    /// Default is 2G.
+    pub max_heap: Option<IntOrString>,
+
+    /// The min heap size for the JVM.
+    /// This is the amount of memory the JVM will start with.
+    /// The JVM will use more memory than this value. The exact amount depends on the JVM implementation.
+    ///
+    /// The value can be a number in bytes or a string with a unit.
+    /// An empty string means no limit.
+    ///
+    /// Default is the same as max heap size. (recommended)
+    ///
+    /// This also sets the requested memory for the container.
+    pub min_heap: Option<IntOrString>,
+
+    /// A margin to add to the max heap size for the JVM overhead like metaspace.
+    ///
+    /// Default is 25% of the max heap size.
+    /// If unsure, leave it at the default.
+    pub jvm_overhead: Option<IntOrString>,
+
+    /// If set to true, the container memory limit will be set to the max heap size + jvm overhead.
+    /// If set to false, no memory limit will be set but only the heap size.
+    ///
+    /// Default is true.
+    pub enable_container_limit: Option<bool>,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
 pub struct MinecraftServerInstallsSpec {
     pub software: Option<MinecraftServerInstallSpecServerSoftware>,
-    pub plugins: Option<Vec<String>>,
+    //TODO install datapacks
+    //TODO install resourcepack
+    //TODO install world
+    //TODO install mods?
+
+    /// A list of plugins to install.
+    ///
+    /// These are the resource ids inside spigotmc.
+    /// They will be automatically placed in the plugins folder.
+    /// Not all resources are downloadable as some are premium or point to external sites which might not be supported.
+    /// The resource id is part of the url of the resource. f.e. `https://www.spigotmc.org/resources/luckperms.28140/` has the id `28140`.
+    ///
+    /// See more at [Itzg's Minecraft Docker Image](https://docker-minecraft-server.readthedocs.io/en/latest/mods-and-plugins/spiget/)
+    /// We might extract the resource id from the url ourselves in the future.
+    pub spigot: Option<Vec<i32>>,
+    /// A list of modrinth resources to install.
+    ///
+    /// This can be plugins, mods, datapacks, etc.
+    /// Thanks to the work of itzg they will be automatically placed in the correct folder.
+    /// Resources are identified by their slug or project id.
+    /// Datapacks are prefixed with `datapack:`.
+    /// See more at [Itzg's Minecraft Docker Image](https://docker-minecraft-server.readthedocs.io/en/latest/mods-and-plugins/modrinth/)
+    pub modrinth: Option<Vec<String>>,
 }
 #[derive(Debug, Clone, Deserialize, Serialize, JsonSchema)]
 #[serde(rename_all = "camelCase", untagged)]
@@ -88,8 +165,8 @@ pub enum MinecraftServerInstallSpecServerSoftware {
         /// Find more information at [PaperMC](https://papermc.io/software/folia).
         folia: String
     },
+    //TODO custom server url
 }
-
 #[derive(Debug, Clone, Deserialize, Serialize, JsonSchema)]
 #[serde(rename_all = "camelCase")]
 pub struct MinecraftServerPersistence {
